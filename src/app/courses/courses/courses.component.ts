138c463e447705f381dc848a64b9256904ecb71d
Wesley S. Batista <wesleysb07@gmail.com>
2021-10-26 00:21:18 -0300

    Aula 6 - Criando Material Table para listar os cursos.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  public courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Front-end' }
+  ];
+  public displayedColumns: string[] = ['name', 'category'];
+
+  constructor() {
+    // this.courses = [];
+  }
 
   ngOnInit(): void {
   }
