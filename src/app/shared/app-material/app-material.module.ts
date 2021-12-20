950693fd5d576c424ccb8fafaa6720fc1ac186ef
Wesley S. Batista <wesleysb07@gmail.com>
2021-12-19 21:21:41 -0300

    Utilizando Pipe para mostrar icones.

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class AppMaterialModule { }
